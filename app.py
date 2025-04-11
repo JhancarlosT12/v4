01e8baa08b46934720ab67ef917700d9d09e8c84
Jhancarlos T. Castaño <132229326+JhancarlosT12@users.noreply.github.com>
2025-04-11 07:55:21 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1160,4 +1160,5 @@ async def health_check():
 # Punto de entrada para ejecutar la aplicación
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
+    print(f"Starting server on port {port}")
     uvicorn.run("app:app", host="0.0.0.0", port=port, reload=True)
